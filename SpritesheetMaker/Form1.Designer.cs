dc345be7a7694c71d32a2390327ac5b319674520
LizbeeFrisbee <cryliaaa@gmail.com>
2025-09-23 10:56:44 +0200

    Add project files.

---
+++ SpritesheetMaker/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace SpritesheetMaker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.zoomLevel = new System.Windows.Forms.Label();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageRes = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Minimize = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Maximize = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ResetButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "Load Images";
+            this.button1.BackColor = System.Drawing.Color.GhostWhite;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(45, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load Images";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.SelectImages_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "meow";
+            this.openFileDialog1.Filter = "(*.png;*.TIFF)|*png;*.TIFF";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "Please select your image files";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.trackBar1.Location = new System.Drawing.Point(45, 116);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(144, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackBar1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseUp);
+            // 
+            // zoomLevel
+            // 
+            this.zoomLevel.AutoSize = true;
+            this.zoomLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.zoomLevel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.zoomLevel.Location = new System.Drawing.Point(195, 116);
+            this.zoomLevel.Name = "zoomLevel";
+            this.zoomLevel.Size = new System.Drawing.Size(18, 20);
+            this.zoomLevel.TabIndex = 5;
+            this.zoomLevel.Text = "1";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveButton.BackColor = System.Drawing.Color.GhostWhite;
+            this.SaveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.SaveButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.SaveButton.Location = new System.Drawing.Point(1102, 636);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(144, 32);
+            this.SaveButton.TabIndex = 7;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = false;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "(*.png ; *.TIFF) | *.png ; *.TIFF";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(42, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Current Image Size:";
+            // 
+            // imageRes
+            // 
+            this.imageRes.AutoSize = true;
+            this.imageRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.imageRes.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.imageRes.Location = new System.Drawing.Point(194, 164);
+            this.imageRes.Name = "imageRes";
+            this.imageRes.Size = new System.Drawing.Size(19, 20);
+            this.imageRes.TabIndex = 8;
+            this.imageRes.Text = "--";
+            this.imageRes.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.Minimize);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.Exit);
+            this.panel1.Controls.Add(this.Maximize);
+            this.panel1.Location = new System.Drawing.Point(12, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1240, 34);
+            this.panel1.TabIndex = 9;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Minimize
+            // 
+            this.Minimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Minimize.BackColor = System.Drawing.Color.SlateGray;
+            this.Minimize.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.Minimize.ForeColor = System.Drawing.SystemColors.Control;
+            this.Minimize.Location = new System.Drawing.Point(1140, 1);
+            this.Minimize.Margin = new System.Windows.Forms.Padding(0);
+            this.Minimize.Name = "Minimize";
+            this.Minimize.Size = new System.Drawing.Size(32, 31);
+            this.Minimize.TabIndex = 1;
+            this.Minimize.Text = "-";
+            this.Minimize.UseVisualStyleBackColor = false;
+            this.Minimize.Click += new System.EventHandler(this.Minimize_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(3, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Spritesheet Maker";
+            // 
+            // Exit
+            // 
+            this.Exit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Exit.BackColor = System.Drawing.Color.SlateGray;
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.Exit.ForeColor = System.Drawing.SystemColors.Control;
+            this.Exit.Location = new System.Drawing.Point(1204, 1);
+            this.Exit.Margin = new System.Windows.Forms.Padding(0);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(32, 31);
+            this.Exit.TabIndex = 1;
+            this.Exit.Text = "X";
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Maximize
+            // 
+            this.Maximize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Maximize.BackColor = System.Drawing.Color.SlateGray;
+            this.Maximize.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.Maximize.ForeColor = System.Drawing.SystemColors.Control;
+            this.Maximize.Location = new System.Drawing.Point(1172, 1);
+            this.Maximize.Margin = new System.Windows.Forms.Padding(0);
+            this.Maximize.Name = "Maximize";
+            this.Maximize.Size = new System.Drawing.Size(32, 31);
+            this.Maximize.TabIndex = 1;
+            this.Maximize.Text = "▢";
+            this.Maximize.UseVisualStyleBackColor = false;
+            this.Maximize.Click += new System.EventHandler(this.Maximize_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel1.BackgroundImage = global::SpritesheetMaker.Properties.Resources.transparent_bigger;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(261, 57);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(985, 573);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.imageRes);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.ResetButton);
+            this.panel2.Controls.Add(this.SaveButton);
+            this.panel2.Controls.Add(this.flowLayoutPanel1);
+            this.panel2.Controls.Add(this.zoomLevel);
+            this.panel2.Controls.Add(this.trackBar1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(1, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1262, 679);
+            this.panel2.TabIndex = 10;
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResetButton.BackColor = System.Drawing.Color.GhostWhite;
+            this.ResetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.ResetButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ResetButton.Location = new System.Drawing.Point(952, 636);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(144, 32);
+            this.ResetButton.TabIndex = 7;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = false;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumBlue;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label zoomLevel;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label imageRes;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Maximize;
+        private System.Windows.Forms.Button Minimize;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button ResetButton;
+    }
+}
+
---
+++ SpritesheetMaker/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Configuration;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SpritesheetMaker
+{
+    public partial class MainForm : Form
+    {
+        private int imageScale;
+        Size initialSize;
+        bool isInitialSizeSet;
+
+        private bool _Moving = false;
+        private Point _Offset;
+        public List<Image> images = new List<Image>();
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public void SelectImages_Click(object sender, EventArgs e)
+        {
+            // Open The File Browser Dialogue
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                
+
+                Image aspectChecker = Image.FromFile(openFileDialog1.FileNames.ElementAt(0));
+                if (!isInitialSizeSet)
+                {
+                    initialSize = aspectChecker.Size;
+                    isInitialSizeSet = true;
+                    imageRes.Text = aspectChecker.Size.Height.ToString();
+                }
+
+                if (aspectChecker.Width == aspectChecker.Height && aspectChecker.Size == initialSize)
+                {
+                    // Read the files
+                    foreach (String file in openFileDialog1.FileNames)
+                    {
+                        // Add to Panel
+                        try
+                        {
+                            Image image = Image.FromFile(file);
+                            images.Add(image);
+                            PictureBoxWithInterpolationMode pictureBox = new PictureBoxWithInterpolationMode();
+                            pictureBox.Width = image.Width;
+                            pictureBox.Height = image.Height;
+                            pictureBox.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                            pictureBox.BackColor = Color.Transparent;
+                            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                            pictureBox.Image = image;
+                            flowLayoutPanel1.Controls.Add(pictureBox);
+                        }
+                        catch (SecurityException ex)
+                        {
+                            // The user lacks appropriate permissions to read files, discover paths, etc.
+                            MessageBox.Show("Security error. Please contact your administrator for details.\n\n" +
+                                "Error message: " + ex.Message + "\n\n" +
+                                "Details (send to Support):\n\n" + ex.StackTrace
+                            );
+                        }
+                        catch (Exception ex)
+                        {
+                            // Could not load the image - probably related to Windows file system permissions.
+                            MessageBox.Show("Cannot display the image: " + file.Substring(file.LastIndexOf('\\'))
+                                + ". You may not have permission to read the file, or " +
+                                "it may be corrupt.\n\nReported error: " + ex.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid image size.");
+                }
+
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            zoomLevel.Text = trackBar1.Value.ToString();
+        }
+
+        private void trackBar1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            int currentValue = trackBar1.Value;
+            imageScale = currentValue;
+
+
+            if (flowLayoutPanel1.HasChildren)
+            {
+                foreach (PictureBox tempBox in flowLayoutPanel1.Controls)
+                {
+                    tempBox.Width = initialSize.Width * imageScale;
+                    tempBox.Height = initialSize.Height * imageScale;
+                }
+
+
+            }
+        }
+
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            DialogResult sr = saveFileDialog1.ShowDialog();
+            Stream myStream;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    // Code to write the stream goes here.
+                    //Bitmap myBitmap = new Bitmap(flowLayoutPanel1.Width, flowLayoutPanel1.Height, myGraphics);
+                    myStream.Close();
+                }
+            }
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _Moving = true;
+            _Offset = new Point(e.X, e.Y);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_Moving)
+            {
+                Point newlocation = this.Location;
+                newlocation.X += e.X - _Offset.X;
+                newlocation.Y += e.Y - _Offset.Y;
+                this.Location = newlocation;
+            }
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (_Moving)
+            {
+                _Moving = false;
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Form1_MouseDown(sender, e);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Form1_MouseMove(sender, e);
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Form1_MouseUp(sender, e);
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Maximize_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+            {
+                WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                WindowState = FormWindowState.Normal;
+            }
+            
+        }
+
+        private void Minimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            images.Clear();
+            flowLayoutPanel1.Controls.Clear();
+            isInitialSizeSet = false;
+        }
+    }
+}
